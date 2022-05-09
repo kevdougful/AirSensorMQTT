d8393d363be51ac0ec54e67fea68bacc787bc27c
Kevin <kevdougful@gmail.com>
2022-05-09 11:42:54 -0500

    Use Simple Architecture For Now
    YAGNI.

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,33 @@
-#include <Arduino.h>
-
 #include "Zanshin_BME680.h"
 
-const uint32_t SERIAL_SPEED{115200};
+const uint32_t SERIAL_SPEED{115200};  ///< Set the baud rate for Serial I/O
 
 BME680_Class BME680;
 
-/* SETUP */
 void setup() {
-  // Start serial port at Baud rate
-  Serial.begin(SERIAL_SPEED);
-  Serial.print(F("Starting I2CDemo example program for BME680\n"));
+  Serial.begin(SERIAL_SPEED);  // Start serial port at Baud rate
   Serial.print(F("- Initializing BME680 sensor\n"));
   while (!BME680.begin(I2C_STANDARD_MODE)) {
     // Start BME680 using I2C, use first device found
     Serial.print(F("-  Unable to find BME680. Trying again in 5 seconds.\n"));
     delay(5000);
-  } 
+  }  // of loop until device is located
   Serial.print(F("- Setting 16x oversampling for all sensors\n"));
   BME680.setOversampling(TemperatureSensor, Oversample16);
   BME680.setOversampling(HumiditySensor, Oversample16);
   BME680.setOversampling(PressureSensor, Oversample16);
   Serial.print(F("- Setting IIR filter to a value of 4 samples\n"));
   BME680.setIIRFilter(IIR4);
-  Serial.print(F("- Setting gas measurement to 320\xC2\xB0\x43 for 150ms\n"));  // "�C" symbols
-  BME680.setGas(320, 150);  // 32C for 150 milliseconds
+  Serial.print(F("- Setting gas measurement to 320\xC2\xB0\x43 for 150ms\n"));
+  BME680.setGas(320, 150);
 }
 
-/* LOOP */
 void loop() {
-  Serial.print("hello");
-  delay(1000);
+  static int32_t  temp, humidity, pressure, gas;  // BME readings
+  static char     buf[16];                        // sprintf text buffer
+
+  BME680.getSensorData(temp, humidity, pressure, gas);  // Get readings
+
+  // Take it easy for a bit
+  delay(2000);
 }
